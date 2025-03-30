2fa73e34042ccec680b6d3bf922c33de084d87cf
Samuel Nnanwobu <nnanwobus@gmail.com>
2025-03-30 20:02:59 +0100

    second commi3

--- api/user.js
+++ api/user.js
@@ -1,6 +1,7 @@
 const app = require('../app');
 const userRouter = require('../routers/userRouter');
 
-app.use('/api/v2/users', userRouter);
+// app.use('/api/v2/users', userRouter);
+app.use('/api/users', userRouter);
 
 module.exports = app;
