47b5f20258315c94183e931f160716eba0ce22bd
Samuel Nnanwobu <nnanwobus@gmail.com>
2025-03-31 20:28:05 +0100

    second commi5

---
+++ mongodb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// dotenv.config({ path: './config.env' });
+const DB = process.env.CONNECTION_STRING.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log(' Remote connection established...');
+  });
