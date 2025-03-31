897563cfb7730fb5e34b48f664ddb026c413270c
Samuel Nnanwobu <nnanwobus@gmail.com>
2025-03-31 15:20:50 +0100

    second commi4

--- api/reply.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app');
-const replyRouter = require('../routers/replyRouter');
-// app.use('/api/v2/replies', replyRouter);
-app.use('/api/reply', replyRouter);
-
-module.exports = app;
--- api/user.js
+++
@@ -1,7 +0,0 @@
-const app = require('../app');
-const userRouter = require('../routers/userRouter');
-
-// app.use('/api/v2/users', userRouter);
-app.use('/api/user', userRouter);
-
-module.exports = app;
