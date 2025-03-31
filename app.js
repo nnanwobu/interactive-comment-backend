dfaf5df9214c8b939a3dc70a4b68061d256be03c
Samuel Nnanwobu <nnanwobus@gmail.com>
2025-03-31 21:50:25 +0100

    second commi5

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v2/users', userRouter);
 app.use('/api/v2/replies', replyRouter);
-app.use('/api/comment', commentRouter);
+app.use('/api/v2/comments', commentRouter);
 app.all('*', (req, res, next) => {
   const err = new AppError(`can't find ${req.originalUrl} on this server`, 404);
   next(err);
