c6f567337abd984d8b64f7d33efe324a5962fc17
Samuel Nnanwobu <nnanwobus@gmail.com>
2025-04-01 10:35:41 +0100

    second commi5

--- server.js
+++ server.js
@@ -13,17 +13,16 @@ const DB = process.env.CONNECTION_STRING.replace(
   '<db_password>',
   process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    // console.log(con.connections);
-    console.log(' Remote connection established at this our...');
-  });
+// {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// }
+mongoose.connect(DB).then((con) => {
+  // console.log(con.connections);
+  console.log(' Remote connection established at this our...');
+});
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
